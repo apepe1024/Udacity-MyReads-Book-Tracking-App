19221caa3e4d0cf6ad727a8f6d1dde0ba086e71d
Alessandro Pepe <apepe1024@gmail.com>
2018-06-16 12:09:55 -0400

    initial commit upon project completion

---
+++ README.md
@@ -0,0 +1,30 @@
+# Udacity My Reads App
+
+This is the mid-way project for the "Building With React" section of the Udacity Front-End Web Developer Nanodegree Program.
+
+## Getting Started
+
+### Install
+```
+$ npm install
+```
+### Run App
+```
+$ npm start
+```
+### View application
+```
+http://localhost:3000
+```
+
+## Prerequisites
+
+* [Node.js](https://nodejs.org/en/)
+* [npm](https://www.npmjs.com/)
+
+## Authors
+
+* **Alessandro Pepe** (https://github.com/apepe1024)
+
+#### License
+The contents of this repository are covered under the [MIT License](https://choosealicense.com/licenses/mit/).
---
+++ src/components/search.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import DebounceInput from 'react-debounce-input'
+import BookShelf from './BookShelf'
+
+class SearchBook extends Component {
+  componentDidMount() {
+    this.props.emptybooks()
+  }
+  render() {
+    return <div className="search-books">
+      <div className="search-books-bar">
+        <Link
+          className="close-search"
+          to="/">
+          Close
+          </Link>
+        <div className="search-books-input-wrapper">
+          <DebounceInput
+            debounceTimeout={325}
+            element="input"
+            type="text"
+            value={this.props.books.string}
+            onChange={this.props.searchQuery}
+            placeholder="Search by title or author"
+          />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <BookShelf updateShelf={this.props.updateShelf} shelf="Search Results" books={this.props.books} />
+      </div>
+    </div>
+  };
+};
+
+export default SearchBook;
